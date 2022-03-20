d4db0fe2065cf8515c4a628083f8e1893eee6860
楠瓜Moenen <moenen6@gmail.com>
2022-03-20 21:27:39 +0800

    before block stack

--- Assets/Yaya/Script/Entity/Environment/CheckPoint/eCheckAltar.cs
+++ Assets/Yaya/Script/Entity/Environment/CheckPoint/eCheckAltar.cs
@@ -8,6 +8,45 @@ namespace Yaya {
 	public class eCheckAltar : eCheckPoint {
 
 
+		// SUB
+		[System.Serializable]
+		public class CheckPointData {
+			[System.Serializable]
+			public struct Data {
+				public int ID;
+				public int X;
+				public int Y;
+			}
+			public Data[] CPs = null;
+		}
+
+		// Data
+		private static readonly Dictionary<int, Vector2Int> PositionPool = new();
+
+
+		// MSG
+		public static void Initialize () {
+			var game = Object.FindObjectOfType<Game>();
+			if (game == null) return;
+			try {
+				PositionPool.Clear();
+				var path = Util.CombinePaths(game.MapRoot, $"{Application.productName}.cp");
+				if (Util.FileExists(path)) {
+					var data = JsonUtility.FromJson<CheckPointData>(Util.FileToText(path));
+					if (data != null) {
+						foreach (var cp in data.CPs) {
+							PositionPool.TryAdd(cp.ID, new(cp.X, cp.Y));
+						}
+					}
+				}
+			} catch (System.Exception ex) {
+#if UNITY_EDITOR
+				Debug.LogException(ex);
+#endif
+			}
+		}
+
+
 		private static readonly int[] ARTWORK_CODES = new int[] { "Check Altar 0".AngeHash(), "Check Altar 1".AngeHash(), "Check Altar 2".AngeHash(), "Check Altar 3".AngeHash(), "Check Altar 4".AngeHash(), "Check Altar 5".AngeHash(), "Check Altar 6".AngeHash(), "Check Altar 7".AngeHash(), "Check Altar 8".AngeHash(), "Check Altar 9".AngeHash(), "Check Altar 10".AngeHash(), "Check Altar 11".AngeHash(), "Check Altar 12".AngeHash(), "Check Altar 13".AngeHash(), "Check Altar 14".AngeHash(), "Check Altar 15".AngeHash(), "Check Altar 16".AngeHash(), "Check Altar 17".AngeHash(), "Check Altar 18".AngeHash(), "Check Altar 19".AngeHash(), "Check Altar 20".AngeHash(), "Check Altar 21".AngeHash(), "Check Altar 22".AngeHash(), "Check Altar 23".AngeHash(), };
 		protected override int ArtCode => Data >= 0 && Data < ARTWORK_CODES.Length ? ARTWORK_CODES[Data] : 0;
 
@@ -54,11 +93,10 @@ namespace Yaya.Editor {
 			}
 			// Write Position File
 			try {
-				var cpData = new eCheckPoint.CheckPointData() { CPs = new eCheckPoint.CheckPointData.Data[cpPool.Count] };
+				var cpData = new eCheckAltar.CheckPointData() { CPs = new eCheckAltar.CheckPointData.Data[cpPool.Count] };
 				int index = 0;
 				foreach (var ((typeID, id), pos) in cpPool) {
-					cpData.CPs[index] = new eCheckPoint.CheckPointData.Data() {
-						Type = typeID,
+					cpData.CPs[index] = new eCheckAltar.CheckPointData.Data() {
 						ID = id,
 						X = pos.x,
 						Y = pos.y,
--- Assets/Yaya/Script/Entity/Environment/CheckPoint/eCheckPoint.cs
+++ Assets/Yaya/Script/Entity/Environment/CheckPoint/eCheckPoint.cs
@@ -8,50 +8,13 @@ namespace Yaya {
 	public abstract class eCheckPoint : Entity, IInitialize {
 
 
-		// SUB
-		[System.Serializable]
-		public class CheckPointData {
-			[System.Serializable]
-			public struct Data {
-				public int Type;
-				public int ID;
-				public int X;
-				public int Y;
-			}
-			public Data[] CPs = null;
-		}
-
 
 		// Api
 		public override int Layer => (int)EntityLayer.Environment;
 		protected abstract int ArtCode { get; }
 
 		// Data
-		private static readonly Dictionary<int, Vector2Int> PositionPool = new();
 		private int ArtworkCode = 0;
-
-
-		// MSG
-		public static void Initialize () {
-			var game = Object.FindObjectOfType<Game>();
-			if (game == null) return;
-			try {
-				PositionPool.Clear();
-				var path = Util.CombinePaths(game.MapRoot, $"{Application.productName}.cp");
-				if (Util.FileExists(path)) {
-					var data = JsonUtility.FromJson<CheckPointData>(Util.FileToText(path));
-					if (data != null) {
-						foreach (var cp in data.CPs) {
-							PositionPool.TryAdd(cp.ID, new(cp.X, cp.Y));
-						}
-					}
-				}
-			} catch (System.Exception ex) {
-#if UNITY_EDITOR
-				Debug.LogException(ex);
-#endif
-			}
-		}
 
 
 		public override void OnCreate (int frame) {
