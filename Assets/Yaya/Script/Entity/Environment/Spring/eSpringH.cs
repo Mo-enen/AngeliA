c2996de43627ad5ddd57b6cccd1fe92072238f9e
楠瓜Moenen <moenen6@gmail.com>
2022-04-09 22:34:01 +0800

    49

--- Assets/Yaya/Script/Entity/Environment/Spring/eSpringH.cs
+++ Assets/Yaya/Script/Entity/Environment/Spring/eSpringH.cs
@@ -1,6 +1,7 @@
 using AngeliaFramework;
 
 namespace Yaya {
+	[DefaultEntityData(96)]
 	public class eSpringH : eSpring {
 		protected override bool Horizontal => true;
 	}
--- Assets/Yaya/Script/Entity/Environment/Spring/eSpringV.cs
+++ Assets/Yaya/Script/Entity/Environment/Spring/eSpringV.cs
@@ -1,6 +1,7 @@
 using AngeliaFramework;
 
 namespace Yaya {
+	[DefaultEntityData(96)]
 	public class eSpringV : eSpring {
 		protected override bool Horizontal => false;
 
