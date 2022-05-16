1b9c98ec324f89999bda17db2466ca71579efe89
楠瓜Moenen <moenen6@gmail.com>
2022-05-16 17:58:36 +0800

    516

--- Assets/Yaya/Script/Entity/Environment/Furniture/eFridge.cs
+++ Assets/Yaya/Script/Entity/Environment/Furniture/eFridge.cs
@@ -7,16 +7,14 @@ namespace Yaya {
 	public class eFridge : eFurniture {
 
 		private static readonly int[] CODES = new int[] { "Fridge 0".AngeHash(), };
-		private static readonly int[] CODES_OPEN = new int[] { "Fridge Open 0".AngeHash(), };
+		private static readonly int[] CODES_OPEN = new int[] { "Fridge 0 Open".AngeHash(), };
 
 		protected override Direction3 ModuleType => Direction3.None;
 		protected override int[] ArtworkCodes_LeftDown => CODES;
 		protected override int[] ArtworkCodes_Mid => CODES;
 		protected override int[] ArtworkCodes_RightUp => CODES;
 		protected override int[] ArtworkCodes_Single => Open ? CODES_OPEN : CODES;
-
-		private bool Open = false;
-
+		public bool Open { get; private set; } = false;
 
 	}
 }
--- Assets/Yaya/Script/Entity/Environment/Furniture/eWardrobe.cs
+++ Assets/Yaya/Script/Entity/Environment/Furniture/eWardrobe.cs
@@ -9,12 +9,14 @@ namespace Yaya {
 
 
 		private static readonly int[] CODES = new int[] { "Wardrobe 0".AngeHash(), "Wardrobe 1".AngeHash(), "Wardrobe 2".AngeHash(), "Wardrobe 3".AngeHash(), };
+		private static readonly int[] CODES_OPEN = new int[] { "Wardrobe 0 Open".AngeHash(), "Wardrobe 1 Open".AngeHash(), "Wardrobe 2 Open".AngeHash(), "Wardrobe 3 Open".AngeHash(), };
 
 		protected override Direction3 ModuleType => Direction3.None;
 		protected override int[] ArtworkCodes_LeftDown => CODES;
 		protected override int[] ArtworkCodes_Mid => CODES;
 		protected override int[] ArtworkCodes_RightUp => CODES;
-		protected override int[] ArtworkCodes_Single => CODES;
+		protected override int[] ArtworkCodes_Single => Open ? CODES_OPEN : CODES;
+		public bool Open { get; private set; } = false;
 
 	}
 }
