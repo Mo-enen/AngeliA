07e2c0b01ebbff4ce87d6e2a67ca6050c35425d9
楠瓜Moenen <moenen6@gmail.com>
2023-04-06 20:58:35 +0800

    More Pose;

--- Assets/Yaya/Runtime/Entity/Environment/eSlope.cs
+++ Assets/Yaya/Runtime/Entity/Environment/eSlope.cs
@@ -4,38 +4,38 @@ namespace Yaya {
 
 
 	public class eWoodLogSlopeA : Slope {
-		public override Direction3 DirectionVertical => Direction3.Up;
-		public override Direction3 DirectionHorizontal => Direction3.Right;
+		public override Direction2 DirectionVertical => Direction2.Up;
+		public override Direction2 DirectionHorizontal => Direction2.Right;
 	}
 
 
 	public class eWoodLogSlopeB : Slope {
-		public override Direction3 DirectionVertical => Direction3.Up;
-		public override Direction3 DirectionHorizontal => Direction3.Left;
+		public override Direction2 DirectionVertical => Direction2.Up;
+		public override Direction2 DirectionHorizontal => Direction2.Left;
 	}
 
 
 	public class eBrickWallSlopeA : Slope {
-		public override Direction3 DirectionVertical => Direction3.Up;
-		public override Direction3 DirectionHorizontal => Direction3.Right;
+		public override Direction2 DirectionVertical => Direction2.Up;
+		public override Direction2 DirectionHorizontal => Direction2.Right;
 	}
 
 
 	public class eBrickWallSlopeB : Slope {
-		public override Direction3 DirectionVertical => Direction3.Up;
-		public override Direction3 DirectionHorizontal => Direction3.Left;
+		public override Direction2 DirectionVertical => Direction2.Up;
+		public override Direction2 DirectionHorizontal => Direction2.Left;
 	}
 
 
 	public class eBrickWallSlopeC : Slope {
-		public override Direction3 DirectionVertical => Direction3.Down;
-		public override Direction3 DirectionHorizontal => Direction3.Right;
+		public override Direction2 DirectionVertical => Direction2.Down;
+		public override Direction2 DirectionHorizontal => Direction2.Right;
 	}
 
 
 	public class eBrickWallSlopeD : Slope {
-		public override Direction3 DirectionVertical => Direction3.Down;
-		public override Direction3 DirectionHorizontal => Direction3.Left;
+		public override Direction2 DirectionVertical => Direction2.Down;
+		public override Direction2 DirectionHorizontal => Direction2.Left;
 	}
 
 
