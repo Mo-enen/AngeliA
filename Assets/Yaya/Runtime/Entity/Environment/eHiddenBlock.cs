47700e951c9ecb7897d8161c19636246822a9707
楠瓜Moenen <moenen6@gmail.com>
2023-04-07 21:30:03 +0800

    Limb Rotate;

--- Assets/Yaya/Runtime/Entity/Environment/eHiddenBlock.cs
+++ Assets/Yaya/Runtime/Entity/Environment/eHiddenBlock.cs
@@ -6,7 +6,9 @@ using AngeliaFramework;
 namespace Yaya {
 
 
-	public class eHiddenBlockStone : HiddenBlock { }
+	public class eHiddenBlockStone : HiddenBlock {
+		protected override int ArtworkID => "Stone Tile 3".AngeHash();
+	}
 
 
 }
