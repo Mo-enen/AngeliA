6fcaa752742cef8e3fcbcec5acaae48703e5f669
楠瓜Moenen <moenen6@gmail.com>
2023-05-02 22:25:11 +0800

    52

--- Assets/AngeliA Game/Runtime/Entity/Player/eYaya.cs
+++ Assets/AngeliA Game/Runtime/Entity/Player/eYaya.cs
@@ -6,7 +6,6 @@ using AngeliaFramework;
 namespace System.Runtime.CompilerServices { internal static class IsExternalInit { } }
 
 namespace AngeliaGame {
-	[FirstSelectedPlayer]
 	public class eYaya : Player {
 
 
@@ -52,31 +51,13 @@ namespace AngeliaGame {
 		// MSG
 		public eYaya () {
 
-			// Config
-			MovementWidth.Value = 150;
-			MovementHeight.Value = 384;
-			SquatHeight.Value = 200;
-			DashDuration.Value = 20;
 			RunAccumulation.Value = 0;
-			JumpSpeed.Value = 73;
-			SwimInFreeStyle.Value = false;
-			FirstJumpWithRoll.Value = false;
-			SubsequentJumpWithRoll.Value = true;
-			JumpCount.Value = 2;
 			JumpDownThoughOneway.Value = true;
 			FlyAvailable.Value = true;
-			SlideAvailable.Value = true;
 			SlideOnAnyBlock.Value = true;
-			GrabTopAvailable.Value = true;
-			GrabSideAvailable.Value = true;
-			GrabFlipThroughDownAvailable.Value = true;
-			GrabFlipThroughUp.Value = true;
-			FlyMoveSpeed.Value = 32;
 			FlyGlideAvailable.Value = false;
 
 			BulletName.Value = "YayaPaw";
-			AttackDuration.Value = 12;
-			KeepAttackWhenHold.Value = false;
 			MinimalChargeAttackDuration.Value = 42;
 
 			MaxHP.Value = 4;
