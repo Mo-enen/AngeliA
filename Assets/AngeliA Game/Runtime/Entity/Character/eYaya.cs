93bbcb34194b3a70f181ec67f8cc88f7ace66086
楠瓜Moenen <moenen6@gmail.com>
2023-05-08 21:33:20 +0800

    More Pose Ani;

--- Assets/AngeliA Game/Runtime/Entity/Character/eYaya.cs
+++ Assets/AngeliA Game/Runtime/Entity/Character/eYaya.cs
@@ -8,17 +8,7 @@ namespace System.Runtime.CompilerServices { internal static class IsExternalInit
 namespace AngeliaGame {
 	public class eYaya : Player {
 
-
-		private static readonly int HAIR_FFL_ID = "Yaya.FrontHair.FL".AngeHash();
-		private static readonly int HAIR_FFR_ID = "Yaya.FrontHair.FR".AngeHash();
-		private static readonly int HAIR_BFL_ID = "Yaya.BackHair.FL".AngeHash();
-		private static readonly int HAIR_BFR_ID = "Yaya.BackHair.FR".AngeHash();
-		private static readonly int HAIR_FBL_ID = "Yaya.FrontHair.BL".AngeHash();
-		private static readonly int HAIR_FBR_ID = "Yaya.FrontHair.BR".AngeHash();
-		private static readonly int HAIR_BBL_ID = "Yaya.BackHair.BL".AngeHash();
-		private static readonly int HAIR_BBR_ID = "Yaya.BackHair.BR".AngeHash();
-		private static readonly int FACE_GROUP_ID = "Yaya.Face".AngeHash();
-		private static readonly int FACE_BLINK_ID = "Yaya.Face.Blink".AngeHash();
+		// Pose ID
 		private static readonly int CATEAR_L_ID = "Yaya.CatEarL".AngeHash();
 		private static readonly int CATEAR_R_ID = "Yaya.CatEarR".AngeHash();
 		private static readonly int DAMAGE_EAR_L_ID = "Yaya.CatEarL.Damage".AngeHash();
@@ -30,21 +20,11 @@ namespace AngeliaGame {
 		private static readonly int ATTACK_FACE_ID = "Yaya.Face.Attack".AngeHash();
 		private static readonly int PROPELLER_ID = "Propeller".AngeHash();
 
+		// Api
 		public override CharacterRenderingSolution RenderingSolution => CharacterRenderingSolution.Pose;
-		protected override int FrontHair_FL => HAIR_FFL_ID;
-		protected override int FrontHair_FR => HAIR_FFR_ID;
-		protected override int FrontHair_BL => HAIR_FBL_ID;
-		protected override int FrontHair_BR => HAIR_FBR_ID;
-		protected override int BackHair_FL => HAIR_BFL_ID;
-		protected override int BackHair_FR => HAIR_BFR_ID;
-		protected override int BackHair_BL => HAIR_BBL_ID;
-		protected override int BackHair_BR => HAIR_BBR_ID;
-		protected override int FaceGroupID => FACE_GROUP_ID;
-		protected override int FaceBlinkID => FACE_BLINK_ID;
 		protected override bool SpinOnGroundPound => true;
 
 		// Data
-		private readonly YayaSuit DefalutSuit = new();
 		private eGuaGua GuaGua = null;
 
 
@@ -78,6 +58,7 @@ namespace AngeliaGame {
 		protected override void OnPoseCalculated () {
 			base.OnPoseCalculated();
 
+			// Ear
 			if (Head.Tint.a > 0) {
 				bool damaging = AnimatedPoseType == CharacterPoseAnimationType.TakingDamage;
 				DrawAnimalEars(
@@ -87,6 +68,7 @@ namespace AngeliaGame {
 				);
 			}
 
+			// Tail
 			if (
 				!IsPassOut &&
 				AnimatedPoseType != CharacterPoseAnimationType.Sleep &&
@@ -100,12 +82,10 @@ namespace AngeliaGame {
 				}
 			}
 
-			DefalutSuit.Draw(this);
-
 		}
 
 
-		protected override void CalculatePoseFace (int bounce) {
+		protected override void DrawPoseFace (int bounce) {
 			if (!FacingFront) return;
 			var headRect = Head.GetGlobalRect();
 			if (AnimatedPoseType == CharacterPoseAnimationType.TakingDamage) {
@@ -129,81 +109,10 @@ namespace AngeliaGame {
 				DrawFace(ATTACK_FACE_ID, headRect, Head.Width > 0, Head.Z + 1);
 				return;
 			}
-			base.CalculatePoseFace(bounce);
+			base.DrawPoseFace(bounce);
 		}
 
 
 	}
 
-
-	public class YayaSuit : AngeliaFramework.Cloth {
-
-
-		private static readonly int BODY_CODE = "YayaSuit.Body".AngeHash();
-		private static readonly int BODY_CODE_LEFT = "YayaSuit.Body.Left".AngeHash();
-		private static readonly int BODY_CODE_RIGHT = "YayaSuit.Body.Right".AngeHash();
-		private static readonly int BODY_CODE_BACK = "YayaSuit.BodyBack".AngeHash();
-		private static readonly int SOCK_CODE = "YayaSuit.Sock".AngeHash();
-		private static readonly int SHOE_CODE = "YayaSuit.Shoe".AngeHash();
-		private static readonly int SKIRT_CODE = "YayaSuit.Skirt".AngeHash();
-		private static readonly Color32 TINT = new(39, 38, 60, 255);
-
-
-		public override void Draw (Character character) {
-
-			if (character is null) return;
-
-			// Body
-			if (character.Body.Tint.a != 0) {
-				character.Body.Tint = TINT;
-				character.UpperArmL.Tint = TINT;
-				character.UpperArmR.Tint = TINT;
-				character.LowerArmL.Tint = TINT;
-				character.LowerArmR.Tint = TINT;
-				// Skirt
-				DrawSpriteAsSkirt(
-					character, SKIRT_CODE,
-					GetSkirtOffsetY(character),
-					GetSkirtShiftWidth(character)
-				);
-				// Body Cloth
-				if (character.Body.FrontSide) {
-					DrawSpriteAsBody(character.Body, BODY_CODE_LEFT, BODY_CODE, BODY_CODE_RIGHT, character.PoseRootTwist);
-				} else {
-					DrawSpriteAsBody(character.Body, BODY_CODE_BACK);
-				}
-			}
-
-			// Leg
-			if (character.LowerLegL.Tint.a != 0) {
-				DrawSprite(character.LowerLegL, SOCK_CODE);
-			}
-			if (character.LowerLegR.Tint.a != 0) {
-				DrawSprite(character.LowerLegR, SOCK_CODE);
-			}
-
-			// Shoes
-			if (character.FootL.Tint.a != 0) {
-				DrawSpriteAsShoe(character.FootL, SHOE_CODE);
-			}
-			if (character.FootR.Tint.a != 0) {
-				DrawSpriteAsShoe(character.FootR, SHOE_CODE);
-			}
-
-		}
-
-
-		private static int GetSkirtOffsetY (Character character) {
-			return character.MovementState == CharacterMovementState.Run ? Const.CEL / Const.ART_CEL / 2 : 0;
-		}
-
-
-		private static int GetSkirtShiftWidth (Character character) => character.MovementState switch {
-			CharacterMovementState.JumpUp or CharacterMovementState.JumpDown => 2 * Const.CEL / Const.ART_CEL,
-			CharacterMovementState.Run => Const.CEL / Const.ART_CEL,
-			_ => 0,
-		};
-
-
-	}
 }
