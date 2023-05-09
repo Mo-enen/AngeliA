3c8e2df2d148ad6f650102c1f59266b93624c237
楠瓜Moenen <moenen6@gmail.com>
2023-05-09 21:57:19 +0800

    More Pose Ani;

--- Assets/AngeliA Game/Runtime/Entity/Character/eNPC.cs
+++ Assets/AngeliA Game/Runtime/Entity/Character/eNPC.cs
@@ -7,23 +7,39 @@ using AngeliaFramework;
 namespace AngeliaGame {
 
 
-	public class eNormalBoy : NPC { }
+	public class eNormalBoy : NPC {
+		protected override int PoseBodyAbove => 7 * A2G;
+	}
+
 	public class eNormalGirl : NPC { }
-	public class eStreetGangster : NPC { }
-	public class eSchoolBoy : NPC { }
+
+	public class eStreetGangster : NPC {
+		protected override int PoseBodyAbove => 9 * A2G;
+	}
+
+	public class eSchoolBoy : NPC {
+		protected override int PoseBodyAbove => 7 * A2G;
+	}
+
 	public class eNormalLady : NPC {
 		protected override Color32 HairColor => new(207, 123, 60, 255);
+		protected override int PoseBodyAbove => 9 * A2G;
 	}
+
 	public class eHousewife : NPC {
 		protected override Color32 HairColor => new(176, 94, 196, 255);
 	}
+
 	public class eSchoolTeacher : NPC { }
+
 	public class eBusinessElderly : NPC {
 		protected override Color32 HairColor => new(212, 212, 212, 255);
 	}
+
 	public class eBlondWoman : NPC {
 		protected override Color32 HairColor => new(252, 213, 74, 255);
 	}
+
 	public class eBlondMan : NPC {
 		protected override Color32 HairColor => new(252, 213, 74, 255);
 	}
