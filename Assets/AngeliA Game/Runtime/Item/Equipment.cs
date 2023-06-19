1d15fe42831d6d81c33f85731ec2121e8b02e5a6
楠瓜Moenen <moenen6@gmail.com>
2023-06-19 21:49:17 +0800

    More Inventory;

---
+++ Assets/AngeliA Game/Runtime/Item/Equipment.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AngeliaFramework;
+
+
+namespace AngeliaGame {
+
+
+	// Wood
+	public class HelmetWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.Helmet;
+	}
+	public class ArmorWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.BodySuit;
+	}
+	public class GlovesWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.Gloves;
+	}
+	public class ShoesWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.Shoes;
+	}
+	public class SwordWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.Weapon;
+	}
+	public class JewelryWood : Equipment {
+		public override EquipmentType EquipmentType => EquipmentType.Jewelry;
+	}
+
+
+
+
+
+
+
+
+
+}
