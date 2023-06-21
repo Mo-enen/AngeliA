2cf37d51e12eff933be71735ea2c0e22e7ec3f51
楠瓜Moenen <moenen6@gmail.com>
2023-06-21 22:07:01 +0800

    Daily Update;

--- Assets/AngeliA Game/Runtime/Item/Fruit.cs
+++ Assets/AngeliA Game/Runtime/Item/Fruit.cs
@@ -5,35 +5,37 @@ using AngeliaFramework;
 
 namespace AngeliaGame {
 
-	public class iAppleRed : Item { }
-	public class iAppleGreen : Item { }
-	public class iPear : Item { }
-	public class iPeach : Item { }
-	public class iBanana : Item { }
-	public class iOrange : Item { }
-	public class iGrapePurple : Item { }
-	public class iGrapeGreen : Item { }
-	public class iStrawberry : Item { }
-	public class iBlueberry : Item { }
-	public class iLemon : Item { }
-	public class iCherry : Item { }
-	public class iPitaya : Item { }
-	public class iApricot : Item { }
-	public class iHoneydew : Item { }
-	public class iAppleRedCut : Item { }
-	public class iAppleGreenCut : Item { }
-	public class iPearCut : Item { }
-	public class iPeachCut : Item { }
-	public class iBananaCut : Item { }
-	public class iOrangeCut : Item { }
-	public class iGrapePurpleCut : Item { }
-	public class iGrapeGreenCut : Item { }
-	public class iStrawberryCut : Item { }
-	public class iBlueberryCut : Item { }
-	public class iLemonCut : Item { }
-	public class iCherryCut : Item { }
-	public class iPitayaCut : Item { }
-	public class iApricotCut : Item { }
-	public class iHoneydewCut : Item { }
+	public abstract class iFruit : Item { }
+
+	public class iAppleRed : iFruit { }
+	public class iAppleGreen : iFruit { }
+	public class iPear : iFruit { }
+	public class iPeach : iFruit { }
+	public class iBanana : iFruit { }
+	public class iOrange : iFruit { }
+	public class iGrapePurple : iFruit { }
+	public class iGrapeGreen : iFruit { }
+	public class iStrawberry : iFruit { }
+	public class iBlueberry : iFruit { }
+	public class iLemon : iFruit { }
+	public class iCherry : iFruit { }
+	public class iPitaya : iFruit { }
+	public class iApricot : iFruit { }
+	public class iHoneydew : iFruit { }
+	public class iAppleRedCut : iFruit { }
+	public class iAppleGreenCut : iFruit { }
+	public class iPearCut : iFruit { }
+	public class iPeachCut : iFruit { }
+	public class iBananaCut : iFruit { }
+	public class iOrangeCut : iFruit { }
+	public class iGrapePurpleCut : iFruit { }
+	public class iGrapeGreenCut : iFruit { }
+	public class iStrawberryCut : iFruit { }
+	public class iBlueberryCut : iFruit { }
+	public class iLemonCut : iFruit { }
+	public class iCherryCut : iFruit { }
+	public class iPitayaCut : iFruit { }
+	public class iApricotCut : iFruit { }
+	public class iHoneydewCut : iFruit { }
 
 }
