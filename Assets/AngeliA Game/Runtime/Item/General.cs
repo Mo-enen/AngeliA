731dceae700ae90fb899f7780f3a54d54d55bbb7
楠瓜Moenen <moenen6@gmail.com>
2023-07-16 20:41:32 +0800

    update

--- Assets/AngeliA Game/Runtime/Item/General.cs
+++ Assets/AngeliA Game/Runtime/Item/General.cs
@@ -6,7 +6,16 @@ using AngeliaFramework;
 namespace AngeliaGame {
 
 
-
+	public class iItemCoin : Item {
+		public static readonly int TYPE_ID = typeof(iItemCoin).AngeHash();
+		public override bool TouchToCollect => true;
+		public override bool ConsumeOnCollect => true;
+		public override void OnCollect (Entity target) {
+			base.OnCollect(target);
+			if (target == null || !Inventory.HasID(target.TypeID)) return;
+			Inventory.AddCoin(target.TypeID, 1);
+		}
+	}
 
 
 
