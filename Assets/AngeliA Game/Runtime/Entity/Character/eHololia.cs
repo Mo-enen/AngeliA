272df3f48a0db6879e3f9d06dbbd925e40efdd2b
楠瓜Moenen <moenen6@gmail.com>
2023-09-23 20:58:45 +0800

    More Hair;

--- Assets/AngeliA Game/Runtime/Entity/Character/eHololia.cs
+++ Assets/AngeliA Game/Runtime/Entity/Character/eHololia.cs
@@ -152,52 +152,78 @@ namespace AngeliaGame {
 	// Hair
 	public class SoraHair : AutoSpriteHair { }
 	public class RobocoHair : AutoSpriteHair { }
-	public class MikoHair : AutoSpriteHair { }
+	public class MikoHair : AutoSpriteBraidHair { }
 	public class SuiseiHair : AutoSpriteHair { }
 	public class AZKiHair : AutoSpriteHair { }
 	public class MelHair : AutoSpriteHair { }
 	public class FubukiHair : AutoSpriteHair { }
 	public class MatsuriHair : AutoSpriteHair { }
-	public class AkiHair : AutoSpriteHair { }
-	public class AkaiHair : AutoSpriteHair { }
-	public class AquaHair : AutoSpriteHair { }
+	public class AkiHair : AutoSpriteBraidHair { }
+	public class AkaiHair : AutoSpriteBraidHair { }
+	public class AquaHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class ShionHair : AutoSpriteHair { }
 	public class AyameHair : AutoSpriteHair { }
 	public class ChocoHair : AutoSpriteHair { }
 	public class SubaruHair : AutoSpriteHair { }
 	public class MioHair : AutoSpriteHair { }
 	public class OkayuHair : AutoSpriteHair { }
-	public class KoroneHair : AutoSpriteHair { }
-	public class PekoraHair : AutoSpriteHair { }
+	public class KoroneHair : AutoSpriteBraidHair { }
+	public class PekoraHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class FlareHair : AutoSpriteHair { }
 	public class NoelHair : AutoSpriteHair { }
-	public class MarineHair : AutoSpriteHair { }
+	public class MarineHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class KanataHair : AutoSpriteHair { }
 	public class WatameHair : AutoSpriteHair { }
-	public class TowaHair : AutoSpriteHair { }
+	public class TowaHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 900;
+	}
 	public class LunaHair : AutoSpriteHair { }
 	public class CocoHair : AutoSpriteHair { }
 	public class LamyHair : AutoSpriteHair { }
 	public class NeneHair : AutoSpriteHair { }
-	public class BotanHair : AutoSpriteHair { }
+	public class BotanHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class PolkaHair : AutoSpriteHair { }
 	public class RisuHair : AutoSpriteHair { }
 	public class MoonaHair : AutoSpriteHair { }
-	public class IofifteenHair : AutoSpriteHair { }
-	public class OllieHair : AutoSpriteHair { }
-	public class MelfissaHair : AutoSpriteHair { }
-	public class ReineHair : AutoSpriteHair { }
+	public class IofifteenHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 900;
+	}
+	public class OllieHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
+	public class MelfissaHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
+	public class ReineHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 1000;
+	}
 	public class CalliopeHair : AutoSpriteHair { }
 	public class KiaraHair : AutoSpriteHair { }
 	public class InaHair : AutoSpriteHair { }
-	public class GuraHair : AutoSpriteHair { }
+	public class GuraHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class AmeHair : AutoSpriteHair { }
-	public class IRySHair : AutoSpriteHair { }
+	public class IRySHair : AutoSpriteBraidHair { }
 	public class FaunaHair : AutoSpriteHair { }
-	public class KroniiHair : AutoSpriteHair { }
+	public class KroniiHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 1000;
+	}
 	public class MumeiHair : AutoSpriteHair { }
-	public class BaeHair : AutoSpriteHair { }
-	public class SanaHair : AutoSpriteHair { }
+	public class BaeHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 1000;
+	}
+	public class SanaHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class LaplusHair : AutoSpriteHair { }
 	public class LuiHair : AutoSpriteHair { }
 	public class KoyoriHair : AutoSpriteHair { }
@@ -209,7 +235,9 @@ namespace AngeliaGame {
 	public class ShioriHair : AutoSpriteHair { }
 	public class BijouHair : AutoSpriteHair { }
 	public class NerissaHair : AutoSpriteHair { }
-	public class FuwaHair : AutoSpriteHair { }
+	public class FuwaHair : AutoSpriteBraidHair {
+		protected override int PositionAmountY => 500;
+	}
 	public class MocoHair : AutoSpriteHair { }
 	public class AloeHair : AutoSpriteHair { }
 	public class RushiaHair : AutoSpriteHair { }
