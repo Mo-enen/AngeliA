2b7f5f0b409b76d7d5a7d1744af546b17d7a335c
楠瓜Moenen <moenen6@gmail.com>
2023-11-09 23:13:34 +0800

    riptide added

--- AngeliA Game/Assets/Runtime/Angelia.cs
+++ AngeliA Game/Assets/Runtime/Angelia.cs
@@ -5,7 +5,8 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 using AngeliaFramework;
-
+using Riptide.Utils;
+using Riptide;
 
 namespace System.Runtime.CompilerServices { internal static class IsExternalInit { } }
 
@@ -20,11 +21,21 @@ namespace AngeliaGame {
 
 #if UNITY_EDITOR
 
+
+
 		//[UnityEditor.InitializeOnLoadMethod]
 		public static void Test () {
 
-
-
+			//Server server = new Server();
+			//server.Start(7777, 10);
+			//
+			//var msg = Message.Create(MessageSendMode.Reliable, 0);
+			////server.SendToAll();
+			//
+			//
+			//var e = System.Net.Dns.GetHostEntry("www.contoso.com");
+			////e.AddressList[0].Address
+			
 		}
 
 
