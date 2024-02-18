92bc6ac67147a772a2f4e4463ef6a5cef30516f0
Mo-enen <moenen6@gmail.com>
2024-02-18 20:15:12 +0800

    More workflow change

--- AngeliA to Raylib/Runtime/Game.cs
+++ AngeliA to Raylib/Runtime/Game.cs
@@ -49,6 +49,17 @@ public partial class RaylibGame : Game {
 	}
 
 
+
+	public static void ExtractResourceToFile (string resourceName, string filename) {
+		if (System.IO.File.Exists(filename)) return;
+		using var s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+		using var fs = new System.IO.FileStream(filename, System.IO.FileMode.Create);
+		byte[] b = new byte[s.Length];
+		s.Read(b, 0, b.Length);
+		fs.Write(b, 0, b.Length);
+	}
+
+
 	public static void Run () {
 		var game = new RaylibGame();
 		game.InitializeGame();
--- AngeliA to Raylib/Runtime/GameRender.cs
+++ AngeliA to Raylib/Runtime/GameRender.cs
@@ -435,7 +435,7 @@ public partial class RaylibGame {
 				source.Width *= cell.Width.Sign();
 				source.Height *= cell.Height.Sign();
 				Raylib.DrawTexturePro(
-					texture, source, dest.Expand(0.5f),
+					texture, source.Shrink(0.02f), dest.Expand(0.5f),
 					new Vector2(
 						pivotX * dest.Width,
 						pivotY * dest.Height
