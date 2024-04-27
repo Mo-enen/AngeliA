2a70044a7041b8216cebaa9cf0bddec61d110f74
Mo-enen <moenen6@gmail.com>
2024-04-27 18:58:57 +0800

    Publish Works;

--- Test/test.bat
+++
@@ -1,2 +0,0 @@
-
-dotnet publish -c release -p:OutputPath="Build" -p:PublishDir="Publish" -p:DebugType="none" -p:PublishSingleFile="true" -p:SelfContained="true" -p:PublishTrimmed="false" -p:RuntimeIdentifier="win-x64" -p:IncludeAllContentForSelfExtract="false" -p:PublishReadyToRun="false" -p:IncludeNativeLibrariesForSelfExtract="true" -p:EnableCompressionInSingleFile="true"
