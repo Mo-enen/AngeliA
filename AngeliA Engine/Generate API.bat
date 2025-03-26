0d6c3238b99729a2c50be91766179f2851ca1472
Mo-enen <moenen6@gmail.com>
2025-03-26 21:03:48 +0800

    Remove old API files;

--- AngeliA Engine/Generate API.bat
+++
@@ -1,5 +0,0 @@
-
-dotnet build "../AngeliA Framework/" --no-dependencies -p:WarningLevel=1 -p:GenerateDocumentationFile=true -p:DocumentationFile="C:\Data\AngeliA\AngeliA Engine\API\AngeliA Framework.xml"
-dotnet build "../AngeliA Platformer/" --no-dependencies -p:WarningLevel=1 -p:GenerateDocumentationFile=true -p:DocumentationFile="C:\Data\AngeliA\AngeliA Engine\API\AngeliA Platformer.xml"
-
-pause
